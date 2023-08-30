433566036ffc55f38cbf255f504c82e30277ba0c
Dieguidev <diegogaraycullas@gmail.com>
2023-08-30 11:01:03 -0500

    singleton

--- src/12-abstract.ts
+++ src/12-abstract.ts
@@ -1,7 +1,7 @@
 import { Animal, Dog } from "./09-protected";
 
-const animal = new Animal('gody');
-animal.greeting();
+// const animal = new Animal('gody');
+// animal.greeting();
 
 const loca  = new Dog('loca', 'dieguidev');
 loca.greeting();
